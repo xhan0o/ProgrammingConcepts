71e6cd8af4cb40582ffb5a875516f98987ec1a26
xhan0o <shantanoopatel@gmail.com>
2018-08-26 20:03:42 -0400

    parameters added in module

--- Ex_Files_Linux_Kernel/simplemodule.c
+++ Ex_Files_Linux_Kernel/simplemodule.c
@@ -5,13 +5,21 @@
 MODULE_AUTHOR ("xhan0o");
 MODULE_DESCRIPTION ("Very simple loadable module");
 
+static int number = 175;
+static char *word = "hexAF";
+module_param (number, int, S_IRUGO);
+module_param (word, charp, S_IRUGO);
+MODULE_PARM_DESC (number, "A simple int modifiable parameter");
+MODULE_PARM_DESC (word, "A simple string modifiable parameter");
+
 int init_simple(void){
-printk("in init module simple \n");
+printk("number = %d , word = %s \n",number,word);
 return 0;
 //Change return -1 to return an error
 }
+module_init(init_simple);
+
 void cleanup_simple (void){
 printk("in cleanup module simple \n");
 }
-module_init(init_simple);
 module_exit(cleanup_simple);
