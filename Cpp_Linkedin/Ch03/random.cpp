0c06ddad70fe0f976d07a48c1b447b9b69cbec1f
xhan0o <shantanoopatel@gmail.com>
2018-08-30 18:21:24 -0400

    random numbers

---
+++ Cpp_Linkedin/Ch03/random.cpp
@@ -0,0 +1,19 @@
+/*
+ * random.cpp
+ *
+ *  random number
+ * 
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+	srand(time(0));
+	for(int i = 0;i<10;i++)
+	{
+		cout<<rand()<<endl;
+	}
+}
