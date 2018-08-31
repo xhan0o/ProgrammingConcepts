50aa9d711dc6c23dd595e44953e9e17e9e5ca753
xhan0o <shantanoopatel@gmail.com>
2018-08-31 10:25:27 -0400

    Classes and header file

---
+++ Cpp_Linkedin/Ch04/House.cpp
@@ -0,0 +1,67 @@
+/*
+ * House.cpp
+ *	Why Classes?
+ *  Reusablility , encapsulation
+ */
+
+#include "House.h"
+#include <iostream>
+using namespace std;
+
+	//default constructor
+	House::House()
+	{
+		houseColor = "Blue";
+		numBathrooms = 2;
+		numBedrooms = 3;
+		squareFeet = 1200;
+	}
+	House::House(string color, int numBath, int numBed, double sqft)
+	{
+		houseColor = color;
+		numBathrooms = numBath;
+		numBedrooms = numBed;
+		squareFeet = sqft;
+
+	}
+	//destructor method
+	House::~House()
+	{
+		//cout<<"I'm in the destructor"<<endl;
+	}
+
+	//accessor methods
+	string House::getColor()
+	{
+		return houseColor;
+	}
+	int House::getNumBath()
+	{
+		return numBathrooms;
+	}
+	int House::getNumBed()
+	{
+		return numBedrooms;
+	}
+	double House::getSqft()
+	{
+		return squareFeet;
+	}
+
+	//mutator methods
+	void House::setColor(string c)
+	{
+		houseColor = c;
+	}
+	void House::setNumBath(int bath)
+	{
+		numBathrooms = bath;
+	}
+	void House::setNumBed(int bed)
+	{
+		numBedrooms = bed;
+	}
+	void House::setSqft(double sqft)
+	{
+		squareFeet = sqft;
+	}
---
+++ Cpp_Linkedin/Ch04/House.h
@@ -0,0 +1,39 @@
+/*
+ * House.h
+ *
+ *  Header file of class house.
+ */
+#ifndef HOUSE_H_
+#define HOUSE_H_
+#include <string>
+using namespace std;
+
+class House {
+private:
+	string houseColor;
+	int numBathrooms;
+	int numBedrooms;
+	double squareFeet;
+
+public:
+	//constructors
+	House();
+	House(string, int, int, double);
+
+	//destructor
+	~House();
+
+	//accessor methods
+	string getColor();
+	int getNumBath();
+	int getNumBed();
+	double getSqft();
+
+	//mutator methods
+	void setColor(string);
+	void setNumBath(int);
+	void setNumBed(int);
+	void setSqft(double);
+};
+
+#endif /* HOUSE_H_ */
