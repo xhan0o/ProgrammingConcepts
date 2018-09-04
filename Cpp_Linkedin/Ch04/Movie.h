f8234f24815252e8a06131e097f029ef57b67568
xhan0o <shantanoopatel@gmail.com>
2018-09-04 00:44:29 -0400

    Movies class

---
+++ Cpp_Linkedin/Ch04/Movie.h
@@ -0,0 +1,30 @@
+/*
+ * Movie.h
+ *
+ *
+ */
+
+#ifndef MOVIE_H_
+#define MOVIE_H_
+#include <string>
+using namespace std;
+
+class Movie {
+private:
+	string movieName;
+	int year;
+	string rating;
+
+public:
+	Movie();
+	Movie(string, int, string);
+	~Movie();
+	void setName(string);
+	string getName();
+	void setYear(int);
+	int getYear();
+	void setRating(string);
+	string getRating();
+};
+
+#endif /* MOVIE_H_ */
