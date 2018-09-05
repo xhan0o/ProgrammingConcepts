5388e8297a26321386728a8adc961a748e7045cb
xhan0o <shantanoopatel@gmail.com>
2018-09-05 00:31:48 -0400

    catching errors, error handling

---
+++ Cpp_Linkedin/Ch06/errorhandling.cpp
@@ -0,0 +1,33 @@
+/*
+errorhandling.cpp
+handling with error codes
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int errorcode= 10;
+	int numerator;
+	int denominator;
+	do{
+	cout<<"Enter numerator: ";
+	cin>>numerator;
+	cout<<"Enter denominator: ";
+	cin>>denominator;
+	try
+	{
+		if(denominator == 0)
+			throw errorcode;
+		else
+			cout<<"The ratio is: "<<numerator/denominator<<endl;
+	}
+	catch(int error)
+	{
+		if(error == errorcode)
+		{
+			cout<<"\nDivide by zero error!! \n ";
+		}
+	}
+	}while(denominator != 0);
+return 0;
+}
