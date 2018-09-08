226e040e5ca78c60252b53f00dfa668ad578571b
xhan0o <shantanoopatel@gmail.com>
2018-09-08 00:17:22 -0400

    Inheritence and protected variables

--- oop/human/human.cpp
+++ oop/human/human.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 class Human {
-private:
+protected:
   string nameC;
 
 public:
@@ -16,32 +16,20 @@ public:
   }
 };
 
+class ChinesePerson : public Human {
+public:
+  ChinesePerson (const string & name) : Human (name) {  }
+
+  void talkTo(const Human & person) const {
+    cout << nameC << " says: Neih Hou, " << person.name() << "!" << endl;
+  }
+};
+
 int main() {
-  Human joe("joe");
-  Human jennie("jennie");
-  joe.talkTo(jennie);
-  jennie.talkTo(joe);
-
-  Human * pointerToJoe = &joe;
-  Human * pointerToJennie = &jennie;
-
-  pointerToJoe -> talkTo(jennie);
-  pointerToJennie -> talkTo(joe);
-
-  pointerToJoe -> talkTo(*pointerToJennie);
-  pointerToJennie -> talkTo(*pointerToJoe);
-
-  cout << endl << &joe << endl;
-  cout << pointerToJoe << endl;
-  cout << &jennie << endl;
-  cout << pointerToJennie << endl << endl;
-
-  Human & referenceToJoe = *pointerToJoe;
-  Human & referenceToJennie = *pointerToJennie;
-
-  referenceToJoe.talkTo(referenceToJennie);
-  referenceToJennie.talkTo(referenceToJoe);
-
+  Human joe("Joe");
+  ChinesePerson li ("Li");
+  li.talkTo(joe);
+  joe.talkTo(li);
   cout << endl << "Press 'Enter' to exit";
   cin.ignore();
   return 0;
