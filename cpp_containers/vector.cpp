fa1add77b511bfea02d5d476a7380a812790e59a
xhan0o <shantanoopatel@gmail.com>
2018-09-12 16:34:18 -0400

    Copy vector and swap vectors

--- cpp_containers/vector.cpp
+++ cpp_containers/vector.cpp
@@ -3,16 +3,44 @@
 
 using namespace std;
 
+void print_vector( vector <int> vec){
+  for (auto i = vec.begin(); i != vec.end(); ++i){
+         cout << *i << " ";
+  }
+  cout<< endl;
+}
+
 int main(){
   vector<int> v;
+  vector<int> x (5,9);
 
   for (unsigned int i = 1; i<=5; i++){
     v.push_back(i);
   }
+  cout << "v: " << endl;
+  print_vector(v);
 
-  for (auto i = v.begin(); i != v.end(); ++i){
-         cout << *i << " ";
-  }
+  vector<int> v1 (v); // Copy vector by passing constructor
+  cout << "V1: " << endl;
+  print_vector (v1);
+
+  // Copy vecor by in-build copy function
+  vector<int> v2;
+  vector<int> v3;
+  copy(v.begin(),v.end(),back_inserter(v2));
+  v3.assign(v.begin(),v.end());
+  cout << "V2 & V3: " << endl;
+  print_vector (v2);
+  print_vector (v3);
+
+  //Swap Vectros - constant complexity
+  cout << "X & V: " << '\n';
+  print_vector(x);
+  print_vector(v);
+  x.swap(v);
+  cout << "X & V: " << '\n';
+  print_vector(x);
+  print_vector(v);
   cout << endl;
   //Size of vector
   if (!v.empty()){
@@ -22,5 +50,4 @@ int main(){
   cout << "front: " << v.front() << endl;
   cout << "back: " << v.back() << endl;
 }
-
 }
